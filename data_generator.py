5ccc17d7421b898ad7dc982f151d7c6222a45659
Supervico0097 <vic_lilkhoury@hotmail.com>
2023-08-06 21:55:53 +0200

    Add files via upload

--- data_generator.py
+++ data_generator.py
@@ -23,6 +23,9 @@ class SyntheticStreamingDataGenerator:
         self.artist_list = []
         self.song_list = []
         self.stream_list = []
+        self.user_fav_genre = []
+        self.user_fav_artist = []
+        self.user_fav_song = []
 
         with open(config_file_path) as fh:
             data = yaml.load(fh, Loader=yaml.FullLoader)
@@ -38,14 +41,16 @@ class SyntheticStreamingDataGenerator:
 
         self.p_favorite = data['p_favorite']
         self.p_favorite_playlist = data['p_favorite_playlist']
-        self.p_repeat_fav_sg = data['p_repeat_fav_sg']
         self.p_random_songs_stream = data['p_random_songs_stream']
         self.avg_songs_unsub = data['avg_songs_unsub']
         self.avg_songs_sub = data['avg_songs_sub']
-        self.p_1 = data['p_1']
-        self.p_2 = data['p_2']
-        self.p_3 = data['p_3']
-        self.p_4 = data['p_4']
+        self.p_fav_art_sng_gnr_cnt = data['p_fav_art_sng_gnr_cnt']
+        self.p_fav_art_sng_gnr = data['p_fav_art_sng_gnr']
+        self.p_fav_gnr = data['p_fav_gnr']
+        self.p_other = data['p_other']
+        self.min_streams_to_favorites = data['min_streams_to_favorites']
+        self.min_streams_to_famous_song = data['min_streams_to_famous_song']
+        self.min_streams_to_famous_artist = data['min_streams_to_famous_artist']
 
     def run(self):
         for week_no in range(self.weeks_num):
@@ -54,7 +59,10 @@ class SyntheticStreamingDataGenerator:
             self.create_users(week_no)
             self.generate_streams(week_no)
 
+
+        self.generate_additional_tables()
         users_df = pd.DataFrame(self.user_list)
+        users_df.drop(columns=["favorite genres", "favorite artists", "favorite songs", "streams"], inplace=True)
         artist_df = pd.DataFrame(self.artist_list)
         song_df = pd.DataFrame(self.song_list)
         stream_df = pd.DataFrame(self.stream_list)
@@ -62,6 +70,8 @@ class SyntheticStreamingDataGenerator:
         artist_df.to_csv(DATA_PATH / 'artists.csv', index=False)
         song_df.to_csv(DATA_PATH / 'songs.csv', index=False)
         stream_df.to_csv(DATA_PATH / 'streams.csv', index=False)
+
+
 
     def create_users(self, week_no):
         # https://mycurvefit.com/
@@ -135,7 +145,7 @@ class SyntheticStreamingDataGenerator:
                         user['favorite songs'].append(song['song_id'])
 
             # then go randomly through songs
-            for _ in range(20):
+            for _ in range(15):
                 if self.song_list:
                     random_song = random.choice(self.song_list)
 
@@ -161,6 +171,7 @@ class SyntheticStreamingDataGenerator:
                     a=self.probabilities_genre_df.index.to_list(),
                     p=self.probabilities_genre_df.artist.to_list()),
                 'is_famous': _rand_bool(0.1),
+                'number_of_streams': 0,
                 'week_no_created': week_no
             }
             self.artist_list.append(artist)
@@ -188,77 +199,78 @@ class SyntheticStreamingDataGenerator:
         # There will be a Spotify playlist that will consist songs that will appear
         # randomly on users streams regardless of their preferences.
         # weekly_playlist = []
-        premium_songs = [song for song in self.song_list if song['is_premium']]  # create a list of premium songs
         assert self.user_list, "user_list is empty"  # It checks whether the list is empty
+        premium_songs = [song for song in self.song_list if song['is_premium']]  # create a list of premium songs
+
         for user_i in range(len(self.user_list)):
             # count number of songs users goes through
-            if self.user_list[user_i]['is_subscribed']:
-                list_songs = self.song_list
-                if len(self.song_list) < self.avg_songs_sub:
-                    n_songs = len(self.song_list)
-                else:
-                    n_songs = random.randrange(self.avg_songs_sub - int(0.5 * self.avg_songs_sub),
-                                               self.avg_songs_sub + int(0.5 * self.avg_songs_sub))
-            else:
+            list_songs = self.song_list
+            n_songs = len(self.song_list)
+
+            if self.user_list[user_i]['is_subscribed'] and len(self.song_list) > self.avg_songs_sub:
+                n_songs = random.randrange(self.avg_songs_sub - int(0.5 * self.avg_songs_sub),
+                                           self.avg_songs_sub + int(0.5 * self.avg_songs_sub))
+
+            elif not self.user_list[user_i]['is_subscribed'] and len(premium_songs) < self.avg_songs_unsub:
                 list_songs = premium_songs
-                if len(premium_songs) < self.avg_songs_unsub:
-                    n_songs = len(premium_songs)
+                n_songs = len(premium_songs)
+
+            elif not self.user_list[user_i]['is_subscribed'] and len(premium_songs) > self.avg_songs_unsub:
+                list_songs = premium_songs
+                n_songs = random.randrange(self.avg_songs_unsub - int(0.5 * self.avg_songs_unsub),
+                                           self.avg_songs_unsub + int(0.5 * self.avg_songs_unsub))
+
+            # Random songs outside user's favorites
+            if random.uniform(0,1) < self.p_random_songs_stream and list_songs:  # check if user will access the random songs
+
+                for _ in range(int(n_songs)):  # go through songs
+                    random_song = random.choice(list_songs)  # choose random song
+
+                    # if singer is famous, song is famous, same genre, same continent
+                    if (
+                            self.artist_list[random_song['artist_id']]['is_famous']) \
+                            and (random_song['is_famous']) \
+                            and (
+                            random_song['genre'] in self.user_list[user_i]['favorite genres']) \
+                            and (
+                            self.artist_list[random_song['artist_id']]['continent'] == self.user_list[user_i][
+                        'continent']) \
+                            and random.uniform(0, 1) < self.p_fav_art_sng_gnr_cnt:
+                        user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
+                        self.user_list[user_i] = user
+
+
+                    # if singer is famous, song is famous, same genre
+                    elif self.artist_list[random_song['artist_id']]['is_famous'] and random_song['is_famous'] and (
+                            random_song['genre'] in self.user_list[user_i]['favorite genres']) and random.uniform(0,
+                                                                                                                  1) < self.p_fav_art_sng_gnr:
+                        user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
+                        self.user_list[user_i] = user
+
+                    # if singer is famous, song is famous, same genre
+                    elif (random_song['genre'] in self.user_list[user_i]['favorite genres']) and random.uniform(0,
+                                                                                                                1) < self.p_fav_gnr:
+                        user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
+                        self.user_list[user_i] = user
+
+                    elif random.uniform(0, 1) < self.p_other:
+                        user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
+                        self.user_list[user_i] = user
+
+            if random.uniform(0,1) < self.p_favorite_playlist and self.user_list[user_i]['favorite songs']: #check if user will access favorite songs
+                list_songs = self.user_list[user_i]['favorite songs']
+                if len(list_songs) > 1:
+                    n_songs = random.randrange(len(list_songs) - int(0.5 * len(list_songs)),
+                                                len(list_songs) + int(0.5 * len(list_songs)))
                 else:
-                    n_songs = random.randrange(self.avg_songs_unsub - int(0.5 * self.avg_songs_unsub),
-                                               self.avg_songs_unsub + int(0.5 * self.avg_songs_unsub))
-
-            # Random songs outside his favourites
-            if random.uniform(0, 1) < self.p_random_songs_stream:  # check if user will access the random songs
-                if list_songs:
-                    for _ in range(int(n_songs)):  # go through songs
-                        random_song = random.choice(list_songs)  # choose random song
-
-                        # if singer is famous, song is famous, same genre, same continent
-                        if (
-                                random_song['is_artist_famous']) \
-                                and (random_song['is_famous']) \
-                                and (
-                                random_song['genre'] in self.user_list[user_i]['favorite genres']) \
-                                and (
-                                self.artist_list[random_song['artist_id']]['continent'] == self.user_list[user_i][
-                            'continent']) \
-                                and random.uniform(0, 1) < self.p_1:
-                            user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
-                            self.user_list[user_i] = user
-
-                        # if singer is famous, song is famous, same genre
-                        elif random_song['is_artist_famous'] and random_song['is_famous'] and (
-                                random_song['genre'] in self.user_list[user_i]['favorite genres']) and random.uniform(0,
-                                                                                                                      1) < self.p_2:
-                            user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
-                            self.user_list[user_i] = user
-                        # if singer is famous, song is famous, same genre
-                        elif (random_song['genre'] in self.user_list[user_i]['favorite genres']) and random.uniform(0,
-                                                                                                                    1) < self.p_3:
-                            user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
-                            self.user_list[user_i] = user
-
-                        elif random.uniform(0, 1) < self.p_4:
-                            user = self.add_stream(self.user_list[user_i], random_song['song_id'], week_no)
-                            self.user_list[user_i] = user
-
-    '''
-            # favorite songs
-            if random.uniform(0,1) < self.p_favorite_playlist: #check if user will access favourites
-                for i in self.user_list[user_i]['favorite songs']: #go through random songs
-
-                    if random.uniform(0,1) < self.p_favorite: #check if user will listen to favorite
-                        user = self.add_stream(self.user_list[user_i], i)
-                        user_list[user_i] = user
-
-                        while random.uniform(0,1) < self.p_repeat_fav_sg: #check if user will repeat favourite song
-                            user = self.add_stream(self.user_list[user_i], i)
-                            user_list[user_i] = user
+                    n_songs = len(list_songs)
 
 
-    '''
+                for _ in range(int(n_songs)):
+                    if random.uniform(0,1) < self.p_favorite:
+                        user = self.add_stream(self.user_list[user_i], random.choice(list_songs), week_no)
+                        self.user_list[user_i] = user
 
-    # TODO: going through random songs for favorite genre
 
     def add_stream(self, user, i, week_no):
         stream = {
@@ -267,14 +279,78 @@ class SyntheticStreamingDataGenerator:
             'song_id': i,
             'week_no': week_no
         }
-        # tracking how many times user listened to particular song
+        # tracking how many times user listened to particular song and checking if users should add song to favorites
         if i in user['streams']:
             user['streams'][i] += 1
         else:
             user['streams'][i] = 1
-        # TODO: If user listens for a particular song for more than X times then at this song to his favourites
+
+        if user['streams'][i] > self.min_streams_to_favorites:
+            user['favorite songs'].append(i)
+            user['favorite songs'] = list(dict.fromkeys(user['favorite songs']))  # remove duplicates
+
+
+
+        #tracking number of streams for each song and checking if to add to favorites
+        self.song_list[i]['number_of_streams'] += 1
+
+        if self.song_list[i]['number_of_streams'] > self.min_streams_to_famous_song:
+            self.song_list[i]['is_famous'] = True
+
+
+        #tracking number of streams for each artist and checking if to make famous
+        self.artist_list[self.song_list[i]['artist_id']]['number_of_streams'] += 1
+
+        if self.artist_list[self.song_list[i]['artist_id']]['number_of_streams'] > self.min_streams_to_famous_artist:
+            self.artist_list[self.song_list[i]['artist_id']]['is_famous'] = True
+
+
+
         self.stream_list.append(stream)
         return user
+
+
+
+
+
+
+
+
+
+
+    def generate_additional_tables(self):
+
+        #generate users_favorite_gnres table
+        for user in self.user_list:
+            for genre in user['favorite genres']:
+                row = {'user_id': user['user_id'], 'genre_id': genre}
+                self.user_fav_genre.append(row)
+
+        users_fav_genre_df = pd.DataFrame(self.user_fav_genre)
+        users_fav_genre_df.to_csv(DATA_PATH / 'users_favorite_genre.csv', index=False)
+
+
+        #generate users_favorite_artist table
+        for user in self.user_list:
+            for artist in user['favorite artists']:
+                row = {'user_id': user['user_id'], 'artist_id': artist}
+                self.user_fav_artist.append(row)
+
+        users_fav_artist_df = pd.DataFrame(self.user_fav_artist)
+        users_fav_artist_df.to_csv(DATA_PATH / 'users_favorite_artist.csv', index=False)
+
+
+        #generate users_favorite_song table
+        for user in self.user_list:
+            for song in user['favorite songs']:
+                row = {'user_id': user['user_id'], 'song_id': song}
+                self.user_fav_song.append(row)
+
+        users_fav_song_df = pd.DataFrame(self.user_fav_song)
+        users_fav_song_df.to_csv(DATA_PATH / 'users_favorite_song.csv', index=False)
+
+
+
 
 
 if __name__ == "__main__":
